864d66217ca0c38a93b1afeb97d3846c3e088bb9
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2023-10-09 20:18:19 -0300

    adicionando primeiro projeto
    primeiro projeto e css

---
+++ modulo8/Projeto01/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <meta name="description" content="Descrição do meu website">
+    <title>Projeto 01</title>
+    <link rel="stylesheet" href="estilo/style.css">
+</head>
+<body>
+    <header>
+        <div class="center">
+        <div class="logo">Logomarca</div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>                         
+            </ul>
+        </nav>
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>                         
+            </ul>
+        </nav>
+        </div><!--Center-->
+    </header>
+
+    <section class="banner-principal">
+        <div class="center">
+        <form action="">
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar!">
+        </form>
+        </div><!--center-->
+    </section><!--banerprincipal-->
+
+    <section class="decricao-autor">
+        <div class="center">
+        <div class="w50">
+            <h2>Matheus T. Souza</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+            t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+            consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+            sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor. Sed ut massa sapien. Phasellus ultricies ullamcorper ultrices. Interdum et male
+            suada fames ac ante ipsum primis in faucibus. Aenean purus nulla, efficitur vel ipsum in, iaculis mattis risus. Quisque pharetra lorem sodales pretium iaculis. Mauris magna es
+            t, ultrices at pellentesque et, ultricies ullamcorper neque.
+            </p>
+        </div><!--w50-->
+        <div class="w50">
+            <!--Pegar imagem depois-->
+            <img src="images/faquinhal.png" alt="">
+        </div><!--w50-->
+        </div><!--CENTER-->
+    </section><!--Descricao autor -->
+
+    <section>
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+            <div class="box-epecialidade">
+                <h3>icone1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                    t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                    consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                    sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor. Sed ut massa sapien. Phasellus ultricies ullamcorper ultrices. Interdum et male
+                    suada fames ac ante ipsum primis in faucibus. Aenean purus nulla, efficitur vel ipsum in, iaculis mattis risus. Quisque pharetra lorem sodales pretium iaculis. Mauris magna es
+                    t, ultrices at pellentesque et, ultricies ullamcorper neque.</p>
+            </div>
+        </div><!--Center-->
+    </section>
+
+    <section class="extras">
+        <div class="center">
+            <div class="w50"></div>
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                        t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                        consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                        sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor.</p>
+                        <p class="nome-autor"></p>
+                </div>
+                <div class="depoimento-single">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                        t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                        consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                        sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor.</p>
+                        <p class="nome-autor"></p>
+                </div>
+                <div class="depoimento-single">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                        t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                        consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                        sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor.</p>
+                        <p class="nome-autor"></p>
+                </div>
+            </div>
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos"></div>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                    t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                    consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                    sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                    t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                    consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                    sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae augue velit. Sed urna massa, fringilla et est sed, dignissim semper nulla. Pellentesque habitan
+                    t morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sapien massa, pulvinar vel convallis commodo, tincidunt posuere dui. Ut at elit condimentum, 
+                    consectetur dolor at, ullamcorper purus. Praesent sit amet lorem commodo, pharetra arcu at, posuere lectus. Integer dignissim mollis odio, eu laoreet erat imperdiet in. Nunc
+                    sed eleifend turpis. Pellentesque orci augue, pretium ac mi dignissim, ultrices rutrum dolor.</li>
+                </ul>
+                
+            </div>
+        </div><!--center-->
+    </section>
+
+    <footer>
+        <div class="center">
+            <p>Todos os  direitos reservadoa
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
