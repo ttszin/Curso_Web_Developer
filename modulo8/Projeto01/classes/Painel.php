8e408974463d1b730155b958ac7153c2ba9909c7
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2024-01-28 04:31:24 -0300

    Terminado login e loggout
    Feito login e loggout

--- modulo8/Projeto01/classes/Painel.php
+++ modulo8/Projeto01/classes/Painel.php
@@ -3,5 +3,9 @@
         public static function logado(){
             return isset($_SESSION['login']) ? true : false;
         }
+        public static function loggout(){
+            session_destroy();
+            header('Location: '.INCLUDE_PATH_PAINEL);
+        }
     }
 ?>
--- modulo8/Projeto01/painel/main.php
+++ modulo8/Projeto01/painel/main.php
@@ -1,1 +1,37 @@
-<H2>Bem vindo ao painel <?php echo $_SESSION['user']?></H2>
+<?php
+
+    if(isset($_GET['loggout'])){
+        Painel::loggout();
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH;?>estilo/fontawesome/css/all.css" rel="stylesheet"><!--Adicionando o font awesome-->
+    <title>Painel de Controle</title>
+    <link href="<?php echo INCLUDE_PATH_PAINEL; ?>css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<div class="menu">
+
+</div>
+<header>
+    <div class="center">
+        <div class="menu-btn">
+            <i class="fa fa-bars"></i>
+        </div>
+        <div class="loggout">
+            <a href="<?php echo INCLUDE_PATH_PAINEL ?>?loggout "><span>Sair </span><i class="fa fa-sign-out" aria-hidden="true"></i></a>
+        </div><!--loggout-->
+    </div>
+    <div class="clear"></div>
+</header>
+<div class="clear"></div>
+</body>
+</html>
