c161f6391e86e454055f02036f2af7ab484496ff
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2024-02-29 00:06:09 -0300

    Estilização do editar-usuario
    Feito estilização do editar-usuario

--- modulo8/Projeto01/painel/pages/editar-usuario.php
+++ modulo8/Projeto01/painel/pages/editar-usuario.php
@@ -3,23 +3,25 @@
 
 
     <form method="post">
-        <div class="form-group"></div>
+        <div class="form-group row">
             <label>Nome:</label>
             <input type="text" name="nome" required>
         </div><!--form-group-->
-        
-        <div class="form-group"></div>
+
+        <div class="form-group">
             <label>Senha:</label>
             <input type="text" name="senha" required>
         </div><!--form-group-->
 
-        <div class="form-group"></div>
+        <div class="form-group">
             <label>Imagem:</label>
             <input type="file" name="imagem" required>
         </div><!--form-group-->
 
-        <div class="form-group"></div>
+        <div class="form-group">
             <input type="submit" name="acao" value="Atualizar!">
         </div><!--form-group-->
+
+
     </form>
-</div>
+</div><!--box-content-->
